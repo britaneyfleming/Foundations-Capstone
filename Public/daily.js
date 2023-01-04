8a51d48de243c7a6bdf465d2008704fafdcc7bb0
Britaney Fleming <britaneyrfleming@gmail.com>
2023-01-04 08:18:38 -0600

    Commit new changes to calendar/horoscopes

--- Public/daily.js
+++ Public/daily.js
@@ -1,7 +1,7 @@
 const signs = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces']
 
 $('#signs').html(signs.map(function(sign){
-    return '<div class= "col-4 mb-3 mt-3">' + '<button id= "'+sign+'" class= "sign btn btn-primary">' + sign + '</button> </div>';
+    return '<div class= "col-4 mb-3 mt-3">' + '<button id= "'+sign+'" class= "sign controllers-form-button">' + sign + '</button> </div>';
 } ))
 
 $('button.sign').on('click', function(event){
@@ -11,8 +11,14 @@ $('button.sign').on('click', function(event){
         success:function(data){
             $('#horoscope').html(`
             <ul>
+            <li>Today's Date: ${data.current_date}</li>
+            <li>Zodiac Sign Dates: ${data.date_range}</li>
             <li>Mood: ${data.mood}</li>
-            <li>Mood: ${data.mood}</li>
+            <li>Color: ${data.color}</li>
+            <li>Compatibility: ${data.compatibility}</li>
+            <li>Horoscope: ${data.description}</li>
+            <li>Lucky Number: ${data.lucky_number}</li>
+            <li>Lucky Time: ${data.lucky_time}</li>
             </ul>
         `)
         console.log(data);
---
+++ Public/newCalendar.js
@@ -0,0 +1,4 @@
+const calendar = document.querySelector("#app-calendar");
+
+for (let day = 1; day < 32; day ++)
+console.log(day)
